76f3d08a743d3ab4a7a59cf218532e1a3f19bd73
KC <krkeco@gmail.com>
2020-03-23 11:45:24 -0700

    organize components, add error logging

--- App/Navigator.js
+++ App/Navigator.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   ScrollView,
@@ -11,50 +11,47 @@ import {
 
 import AsyncStorage from '@react-native-community/async-storage';
 
-import LogBooks from './LogBooks.js';
-import Logs from './Logs.js';
-import Menu from './Menu.js';
+import LogBooks from './Components/Log/LogBooks.js';
+import Logs from './Components/Log/Logs.js';
+import Menu from './Components/UI/Menu.js';
 
-const BOOKS_KEY = "books";
+const BOOKS_KEY = 'books';
 
 export default class Navigator extends Component<Props> {
- constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
       showLogBook: false,
       selectedBook: 0,
       books: [],
-    }
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getLogData();
   }
 
   storeData = async () => {
     try {
-      await AsyncStorage.setItem(BOOKS_KEY, JSON.stringify(this.state.books))
+      await AsyncStorage.setItem(BOOKS_KEY, JSON.stringify(this.state.books));
       // alert('set'+JSON.stringify(this.state.books))
     } catch (e) {
-      // saving error
+      console.log('error saving log data:'+e)
     }
-  }
+  };
   getLogData = async () => {
     try {
-      const value = await AsyncStorage.getItem(BOOKS_KEY)
-      if(value !== null) {
-        this.setState({books: JSON.parse(value)});
-      }else{
-       
+      const value = await AsyncStorage.getItem(BOOKS_KEY);
+      if (value !== null) {
+        this.setState({ books: JSON.parse(value) });
+      } else {
       }
-    } catch(e) {
-      // error reading value
+    } catch (e) {
+      console.log('error getting log data:'+e)
     }
-  }
-
+  };
 
   deleteLogAlert = (logIndex) => {
-    
     Alert.alert(
       'Delete Log',
       'Deleting this log means you will lose all the data you stored for it, are you sure you want to do this (this is irreversible).',
@@ -64,16 +61,14 @@ export default class Navigator extends Component<Props> {
           onPress: () => console.log('Cancel Pressed'),
           style: 'cancel',
         },
-       
-        {text: 'Confirmed!', onPress: () => this.deleteLog(logIndex)}
-      ],
-        {cancelable: false},
-    );
 
-  }
+        { text: 'Confirmed!', onPress: () => this.deleteLog(logIndex) },
+      ],
+      { cancelable: false },
+    );
+  };
 
   deleteLogBookAlert = () => {
-    
     Alert.alert(
       'Delete LogBook',
       'Deleting this logbook means you will lose all the data you stored for it, are you sure you want to do this (this is irreversible and super cereal).',
@@ -83,92 +78,85 @@ export default class Navigator extends Component<Props> {
           onPress: () => console.log('Cancel Pressed'),
           style: 'cancel',
         },
-       
-        {text: 'Confirmed!', onPress: () => this.deleteBook()}
-      ],
-        {cancelable: true},
-    );
 
-  }
+        { text: 'Confirmed!', onPress: () => this.deleteBook() },
+      ],
+      { cancelable: true },
+    );
+  };
   deleteBook = () => {
     this.goBack();
-    // alert('deleting'+this.state.selectedBook)
-    let newBook = []
+    let newBook = [];
     this.state.books.map((logbook, index) => {
-      if(index != this.state.selectedBook){
+      if (index != this.state.selectedBook) {
         newBook.push(logbook);
       }
-    this.setState({books: newBook},this.storeData);
+      this.setState({ books: newBook }, this.storeData);
     });
-  }
+  };
 
   deleteLog = (logIndex) => {
-    // let newBooks = this.state.books[this.state.selectedBook].logs.splice(logIndex,1);
-    // this.setState({books: newBooks})
-    let newBook = []
-    let newLogBook = []
-    // let newLogs = []
+    let newBook = [];
+    let newLogBook = [];
 
     this.state.books.map((logbook, index) => {
-      if(index != this.state.selectedBook){
+      if (index != this.state.selectedBook) {
         newBook.push(logbook);
-      }else{
+      } else {
         newLogBook = this.newBook(logbook.name);
-        logbook.logs.map((log,i) =>{
-          if(i != logIndex){
+        logbook.logs.map((log, i) => {
+          if (i != logIndex) {
             newLogBook.logs.push(log);
           }
         });
         newBook.push(newLogBook);
       }
     });
-    this.setState({books: newBook},this.storeData);
-  }
+    this.setState({ books: newBook }, this.storeData);
+  };
 
   newBook = (title) => {
     let newBook = {
-      "name":title,
-      logs: []
-    }
+      name: title,
+      logs: [],
+    };
     return newBook;
-  }
+  };
 
-  selectBook = (bookIndex) =>{
+  selectBook = (bookIndex) => {
     this.setState({
       showLogBook: true,
-      selectedBook: bookIndex})
-  }
+      selectedBook: bookIndex,
+    });
+  };
 
   goBack = () => {
-    this.setState({showLogBook: false});
-  }
+    this.setState({ showLogBook: false });
+  };
 
   render() {
-    let booksView = 
-    <LogBooks
-      newBook={this.newBook}
-      saveData={this.storeData}
-      books={this.state.books}
-      addBook={this.addBook}
-      selectBook={this.selectBook}/>
-
-    if(this.state.showLogBook){
-      booksView = <Logs
-        goBack={this.goBack}
+    let booksView = (
+      <LogBooks
+        newBook={this.newBook}
         saveData={this.storeData}
-        deleteLog={(index)=> this.deleteLogAlert(index)}
-        deleteBook={() => this.deleteLogBookAlert()}
-        logBook = {this.state.books[this.state.selectedBook]}/>
-      
-    }
-
-        // <Menu
-        //   goBack={this.goBack}/>
-    return (
-      <View >
-          {booksView}
-      </View>
+        books={this.state.books}
+        addBook={this.addBook}
+        selectBook={this.selectBook}
+      />
     );
 
+    if (this.state.showLogBook) {
+      booksView = (
+        <Logs
+          goBack={this.goBack}
+          saveData={this.storeData}
+          deleteLog={(index) => this.deleteLogAlert(index)}
+          deleteBook={() => this.deleteLogBookAlert()}
+          logBook={this.state.books[this.state.selectedBook]}
+        />
+      );
+    }
+
+    return <View>{booksView}</View>;
   }
 }
